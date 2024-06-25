bfcfaa80f2a42805340b6f2fac577d8e53602120
beyondray <shnokdog123@gmail.com>
2024-06-25 11:07:50 +0500

    feat: add order status model

---
+++ src/order-status/order-status.model.ts
@@ -0,0 +1,23 @@
+import { BelongsTo, Column, Model, Table } from "sequelize-typescript";
+import { Optional } from "sequelize";
+import { Order, OrderAttributes } from "@/order/order.model";
+
+export interface OrderStatusAttributes {
+    id: number;
+    name: string;
+}
+
+export interface OrderStatusCreationAttributes
+    extends Optional<OrderAttributes, "id"> {}
+
+@Table({ timestamps: false })
+export class OrderStatusModel extends Model<
+    OrderStatusAttributes,
+    OrderStatusCreationAttributes
+> {
+    @Column({ allowNull: false })
+    name: string;
+
+    @BelongsTo(() => Order)
+    orders: Order[];
+}
